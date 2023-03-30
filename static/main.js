7b59c7c2f11fc17711c1e85404c245dcf59836ad
rhydb <rhydb@rhydb.com>
2023-03-30 21:24:42 +0100

    update static files

--- static/main.js
+++ static/main.js
@@ -1,7 +1,12 @@
-// format a number with commas, e.g. 1234 => 1,234
+const addToCart = (item) => {
+    let count = Number.parseInt(localStorage.getItem(item) ?? 0) // the current count or 0 if it is not in the basket;
+    localStorage.setItem(item, count + 1);
+}
+
 const formatNumber = (n) => {
-    return n.toString().split("").reverse().reduce((acc, x, i) => {
-        if (i != n.toString().length - 1 && (i + 1) % 3 == 0) {
+    // format a number with commas (1234 => 1,234)
+    return n.toString().split("").reverse().reduce((acc, x, i, arr) => {
+        if (i != arr.length - 1 && (i + 1) % 3 == 0) {
             x = "," + x;
         }
         return x + acc;
