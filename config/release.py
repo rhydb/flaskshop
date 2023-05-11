89ebcffd97d24fe967940c2d1fb5241a2e7f1f55
rhydb <rhydianbrown554@gmail.com>
2023-05-11 19:40:43 +0100

    use environemnt variables

--- config/release.py
+++ config/release.py
@@ -1,4 +1,8 @@
-from config import secrets
+import os
+
+USERNAME = os.environ["username"]
+PASSWORD = os.environ["password"]
+DATABASE = os.environ["database"]
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{secrets.USERNAME}:{secrets.PASSWORD}@csmysql.cs.cf.ac.uk:3306/{secrets.DATABASE}"
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@csmysql.cs.cf.ac.uk:3306/{DATABASE}"
