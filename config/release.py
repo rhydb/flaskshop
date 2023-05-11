908e06c15bd317440755263223715e37b9ca09b1
rhydb <67209342+rhydb@users.noreply.github.com>
2023-05-11 19:22:46 +0100

    use environment secretkey in release config

--- config/release.py
+++ config/release.py
@@ -5,4 +5,5 @@ PASSWORD = os.environ["password"]
 DATABASE = os.environ["database"]
 
 DEBUG = False
+SECRET_KEY = os.environ["secretkey"]
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USERNAME}:{PASSWORD}@csmysql.cs.cf.ac.uk:3306/{DATABASE}"
